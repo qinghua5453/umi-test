d0b26f3ff2905731d2e4e8b071002ba660e3c979
张健 <jian.zhang@chiefclouds.com>
2024-06-28 11:37:14 +0800

    feat: pubuliu-2

--- src/pages/cycle/Waterfall.tsx
+++ src/pages/cycle/Waterfall.tsx
@@ -6,10 +6,6 @@ function deep_JSON<T>(data: T): T {
 }
 
 // Waterfall.tsx
-export interface waterfallItem {
-  /**高:宽的大致比例，用于每一轮获取数据时的估计高度 */
-  scale: number;
-}
 /**瀑布流组件的props */
 export interface waterfallProps<T> {
   /**本组件的外层的ref。用于监听元素的滚动（谁滚动就填谁） */
@@ -24,13 +20,12 @@ export interface waterfallProps<T> {
   itemRender: (item: T, i: number) => React.ReactNode;
 }
 /**展示瀑布流的组件  */
-export default function Waterfall<T extends waterfallItem>(
-  props: waterfallProps<T>,
-) {
+export default function Waterfall<T>(props: waterfallProps<T>) {
   const { scrollRef, cols = 5, marginX = 30, getList, itemRender } = props;
 
   /**瀑布流最外层的ref */
   const listRef = useRef<HTMLDivElement>(null);
+  const listFlatItemHiddenRef = useRef<(HTMLDivElement | null)[]>([]);
   /**每一列的ref。是个数组 */
   const colRef = useRef<(HTMLDivElement | null)[]>([]);
   /**瀑布流每个模块的宽度。随着窗口大小变化而变化 */
@@ -40,7 +35,8 @@ export default function Waterfall<T extends waterfallItem>(
   const [colList, setColList] = useState(
     Array.from({ length: cols }, () => new Array<T>()),
   ); //要展示的图片列表，二维数组
-  const [colHeight, setColHeight] = useState(new Array<number>(cols).fill(0)); //当前每一列的高度
+  console.log('colList', colList);
+  console.log('listFlatItemHiddenRef', listFlatItemHiddenRef);
 
   /**获取列表数据 */
   const _getList = async () => {
@@ -50,63 +46,74 @@ export default function Waterfall<T extends waterfallItem>(
   /**把获取到的列表，按照规律放入二维数组中。 注，需要监听list的变化，再做这个函数，否则无法获取到最新的colList */
   const listToColList = (list: T[]) => {
     const _colList = deep_JSON(colList); //进行深拷贝
-    const _colHeight = deep_JSON(colHeight);
+    const _colHeight = new Array<number>(cols).fill(0);
     for (let i = 0; i < list.length; i++) {
-      //获取当前最短的列表
+      // 获取当前最短的列表
       let minHeight = Infinity;
       let minHeightindex = 0;
+      const h = listFlatItemHiddenRef.current[i]?.offsetHeight as number;
+      const w = listFlatItemHiddenRef.current[i]?.offsetWidth as number;
+      const scale = h / w;
+      console.log('h', h, 'w', w);
       _colHeight.forEach((k, i) => {
         if (k < minHeight) {
           minHeight = k;
           minHeightindex = i;
         }
       });
-      //加上预估的高度，便于下一个元素正确插入
-      _colHeight[minHeightindex] += imgWidth * list[i].scale; //预估的图片高度，后面会更换为真实高度
+      _colHeight[minHeightindex] += imgWidth * scale; //预估的图片高度，后面会更换为真实高度
       _colList[minHeightindex].push(list[i]);
     }
     setColList(_colList);
-
-    //tip: 计算真实高度的函数，在下面的useEffect中，这样才能保证获取到渲染后的数据
   };
 
   useEffect(() => {
     _getList();
   }, []);
+
   useEffect(() => {
-    listToColList(list); // 需要监听list的变化，再做这个函数，否则无法获取到最新的colList
-  }, [list]);
-  useEffect(() => {
-    //当数据渲染后，再去计算真实高度
-    if (colRef.current) {
-      const newHeight = colRef.current.map((k) => k?.offsetHeight || 0);
-      setColHeight(newHeight);
-    }
-  }, [colList]);
+    setTimeout(() => {
+      if (listFlatItemHiddenRef.current) {
+        listToColList(list);
+      }
+    }, 300);
+  }, [list, listFlatItemHiddenRef]);
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'start',
-        justifyContent: 'space-evenly',
-        textAlign: 'center',
-      }}
-      ref={listRef}
-    >
-      {colList.map((list, listI) => {
-        return (
-          <div
-            key={listI}
-            ref={(r) => (colRef.current[listI] = r)}
-            style={{ width: imgWidth }}
-          >
-            {list.map((k, i) => {
-              return itemRender(k, i);
-            })}
-          </div>
-        );
-      })}
-    </div>
+    // 先隐藏渲染一次，拿到各个dom的真实宽高
+    <>
+      <div style={{ visibility: 'hidden', height: 0 }}>
+        {list.map((k, i) => {
+          return (
+            <div key={i} ref={(r) => (listFlatItemHiddenRef.current[i] = r)}>
+              {itemRender(k, i)}
+            </div>
+          );
+        })}
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'start',
+          justifyContent: 'space-evenly',
+          textAlign: 'center',
+        }}
+        ref={listRef}
+      >
+        {colList.map((list, listI) => {
+          return (
+            <div
+              key={listI}
+              ref={(r) => (colRef.current[listI] = r)}
+              style={{ width: imgWidth }}
+            >
+              {list.map((k, i) => {
+                return itemRender(k, i);
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
--- src/pages/cycle/index.tsx
+++ src/pages/cycle/index.tsx
@@ -1,10 +1,10 @@
-import Waterfall, { waterfallItem } from './Waterfall.tsx';
+import Waterfall from './Waterfall.tsx';
 import { useRef } from 'react';
 
 export default function Home() {
   const scrollRef = useRef<HTMLDivElement>(null);
   /**测试的列表数据 */
-  interface item extends waterfallItem {
+  interface item {
     /**图片路径 */
     src: string;
     /**图片描述 */
@@ -14,67 +14,126 @@ export default function Home() {
     const newList: item[] = [
       {
         src:
+          'https://ts1.cn.mm.bing.net/th/id/R-C.f40ba86561918519b95431a5921e4f5d?rik=9AIbo9AhOYel0w&riu=http%3a%2f%2fwww.quazero.com%2fuploads%2fallimg%2f131210%2f1-131210210248.jpg&ehk=v81JiWKphT%2baLBzbhrxRkTUUwwnhJ5F2PFkm4xn4nEM%3d&risl=&pid=ImgRaw&r=0',
+        text: '测试文字5',
+      },
+      {
+        src:
+          'https://ts1.cn.mm.bing.net/th/id/R-C.0dee2228031e4ef5b03d0c5734aef866?rik=BD%2bnjbFbllVmEQ&riu=http%3a%2f%2fimg.zcool.cn%2fcommunity%2f01cf02554336f10000019ae9df1dad.jpg%403000w_1l_2o_100sh.jpg&ehk=zvcYgjHlqK2U2x9ploUbmiBIk7BewUd6lyA0AIswegQ%3d&risl=&pid=ImgRaw&r=0',
+        text: '测试文字6',
+      },
+      {
+        src:
           'https://p2.music.126.net/va9D07KDeS1ovOYAsoXE9A==/7929677859630995.jpg',
         text: '测试文字',
-        scale: 1,
       },
       {
         src:
           'https://ts1.cn.mm.bing.net/th/id/R-C.b0ea268fa1be279d112489ce83ad4696?rik=qItsh%2fBiy33hlg&riu=http%3a%2f%2fwww.quazero.com%2fuploads%2fallimg%2f140303%2f1-140303215009.jpg&ehk=S6PLWamt%2bMzQV8uO9ugcU5d5M19BpXtCpNz2cRJ7q9M%3d&risl=&pid=ImgRaw&r=0',
         text: '测试文字2',
-        scale: 572 / 982,
       },
       {
         src: 'https://scpic.chinaz.net/files/pic/pic9/202009/apic27858.jpg',
         text: '测试文字3',
-        scale: 581 / 434,
       },
       {
         src:
           'https://ts1.cn.mm.bing.net/th/id/R-C.5245459c4835900f30183bebecb3cb55?rik=koS%2bxytGvrBRHw&riu=http%3a%2f%2fpic.zsucai.com%2ffiles%2f2013%2f0723%2fsdidjj4.jpg&ehk=WJLRakwfHBZS2aO2sK%2bCdh4ijkXwyYijy5Z2BFUdnz4%3d&risl=&pid=ImgRaw&r=0',
         text: '测试文字4',
-        scale: 575 / 356,
       },
       {
         src:
           'https://ts1.cn.mm.bing.net/th/id/R-C.f40ba86561918519b95431a5921e4f5d?rik=9AIbo9AhOYel0w&riu=http%3a%2f%2fwww.quazero.com%2fuploads%2fallimg%2f131210%2f1-131210210248.jpg&ehk=v81JiWKphT%2baLBzbhrxRkTUUwwnhJ5F2PFkm4xn4nEM%3d&risl=&pid=ImgRaw&r=0',
         text: '测试文字5',
-        scale: 578 / 327,
       },
       {
         src:
           'https://ts1.cn.mm.bing.net/th/id/R-C.0dee2228031e4ef5b03d0c5734aef866?rik=BD%2bnjbFbllVmEQ&riu=http%3a%2f%2fimg.zcool.cn%2fcommunity%2f01cf02554336f10000019ae9df1dad.jpg%403000w_1l_2o_100sh.jpg&ehk=zvcYgjHlqK2U2x9ploUbmiBIk7BewUd6lyA0AIswegQ%3d&risl=&pid=ImgRaw&r=0',
         text: '测试文字6',
-        scale: 581 / 868,
+      },
+      {
+        src:
+          'https://ts1.cn.mm.bing.net/th/id/R-C.f40ba86561918519b95431a5921e4f5d?rik=9AIbo9AhOYel0w&riu=http%3a%2f%2fwww.quazero.com%2fuploads%2fallimg%2f131210%2f1-131210210248.jpg&ehk=v81JiWKphT%2baLBzbhrxRkTUUwwnhJ5F2PFkm4xn4nEM%3d&risl=&pid=ImgRaw&r=0',
+        text: '测试文字5',
+      },
+      {
+        src:
+          'https://ts1.cn.mm.bing.net/th/id/R-C.0dee2228031e4ef5b03d0c5734aef866?rik=BD%2bnjbFbllVmEQ&riu=http%3a%2f%2fimg.zcool.cn%2fcommunity%2f01cf02554336f10000019ae9df1dad.jpg%403000w_1l_2o_100sh.jpg&ehk=zvcYgjHlqK2U2x9ploUbmiBIk7BewUd6lyA0AIswegQ%3d&risl=&pid=ImgRaw&r=0',
+        text: '测试文字6',
       },
       {
         src:
           'https://ts1.cn.mm.bing.net/th/id/R-C.b0ea268fa1be279d112489ce83ad4696?rik=qItsh%2fBiy33hlg&riu=http%3a%2f%2fwww.quazero.com%2fuploads%2fallimg%2f140303%2f1-140303215009.jpg&ehk=S6PLWamt%2bMzQV8uO9ugcU5d5M19BpXtCpNz2cRJ7q9M%3d&risl=&pid=ImgRaw&r=0',
         text: '测试文字2',
-        scale: 572 / 982,
       },
       {
         src: 'https://scpic.chinaz.net/files/pic/pic9/202009/apic27858.jpg',
         text: '测试文字3',
-        scale: 581 / 434,
-      },
-      {
-        src:
-          'https://ts1.cn.mm.bing.net/th/id/R-C.5245459c4835900f30183bebecb3cb55?rik=koS%2bxytGvrBRHw&riu=http%3a%2f%2fpic.zsucai.com%2ffiles%2f2013%2f0723%2fsdidjj4.jpg&ehk=WJLRakwfHBZS2aO2sK%2bCdh4ijkXwyYijy5Z2BFUdnz4%3d&risl=&pid=ImgRaw&r=0',
-        text: '测试文字4',
-        scale: 575 / 356,
       },
       {
         src:
           'https://ts1.cn.mm.bing.net/th/id/R-C.f40ba86561918519b95431a5921e4f5d?rik=9AIbo9AhOYel0w&riu=http%3a%2f%2fwww.quazero.com%2fuploads%2fallimg%2f131210%2f1-131210210248.jpg&ehk=v81JiWKphT%2baLBzbhrxRkTUUwwnhJ5F2PFkm4xn4nEM%3d&risl=&pid=ImgRaw&r=0',
         text: '测试文字5',
-        scale: 578 / 327,
       },
       {
         src:
           'https://ts1.cn.mm.bing.net/th/id/R-C.0dee2228031e4ef5b03d0c5734aef866?rik=BD%2bnjbFbllVmEQ&riu=http%3a%2f%2fimg.zcool.cn%2fcommunity%2f01cf02554336f10000019ae9df1dad.jpg%403000w_1l_2o_100sh.jpg&ehk=zvcYgjHlqK2U2x9ploUbmiBIk7BewUd6lyA0AIswegQ%3d&risl=&pid=ImgRaw&r=0',
         text: '测试文字6',
-        scale: 581 / 868,
+      },
+      {
+        src:
+          'https://p2.music.126.net/va9D07KDeS1ovOYAsoXE9A==/7929677859630995.jpg',
+        text: '测试文字',
+      },
+      {
+        src:
+          'https://ts1.cn.mm.bing.net/th/id/R-C.b0ea268fa1be279d112489ce83ad4696?rik=qItsh%2fBiy33hlg&riu=http%3a%2f%2fwww.quazero.com%2fuploads%2fallimg%2f140303%2f1-140303215009.jpg&ehk=S6PLWamt%2bMzQV8uO9ugcU5d5M19BpXtCpNz2cRJ7q9M%3d&risl=&pid=ImgRaw&r=0',
+        text: '测试文字2',
+      },
+      {
+        src:
+          'https://ts1.cn.mm.bing.net/th/id/R-C.5245459c4835900f30183bebecb3cb55?rik=koS%2bxytGvrBRHw&riu=http%3a%2f%2fpic.zsucai.com%2ffiles%2f2013%2f0723%2fsdidjj4.jpg&ehk=WJLRakwfHBZS2aO2sK%2bCdh4ijkXwyYijy5Z2BFUdnz4%3d&risl=&pid=ImgRaw&r=0',
+        text: '测试文字4',
+      },
+      {
+        src:
+          'https://ts1.cn.mm.bing.net/th/id/R-C.f40ba86561918519b95431a5921e4f5d?rik=9AIbo9AhOYel0w&riu=http%3a%2f%2fwww.quazero.com%2fuploads%2fallimg%2f131210%2f1-131210210248.jpg&ehk=v81JiWKphT%2baLBzbhrxRkTUUwwnhJ5F2PFkm4xn4nEM%3d&risl=&pid=ImgRaw&r=0',
+        text: '测试文字5',
+      },
+      {
+        src:
+          'https://ts1.cn.mm.bing.net/th/id/R-C.0dee2228031e4ef5b03d0c5734aef866?rik=BD%2bnjbFbllVmEQ&riu=http%3a%2f%2fimg.zcool.cn%2fcommunity%2f01cf02554336f10000019ae9df1dad.jpg%403000w_1l_2o_100sh.jpg&ehk=zvcYgjHlqK2U2x9ploUbmiBIk7BewUd6lyA0AIswegQ%3d&risl=&pid=ImgRaw&r=0',
+        text: '测试文字6',
+      },
+      {
+        src:
+          'https://ts1.cn.mm.bing.net/th/id/R-C.f40ba86561918519b95431a5921e4f5d?rik=9AIbo9AhOYel0w&riu=http%3a%2f%2fwww.quazero.com%2fuploads%2fallimg%2f131210%2f1-131210210248.jpg&ehk=v81JiWKphT%2baLBzbhrxRkTUUwwnhJ5F2PFkm4xn4nEM%3d&risl=&pid=ImgRaw&r=0',
+        text: '测试文字5',
+      },
+      {
+        src:
+          'https://ts1.cn.mm.bing.net/th/id/R-C.0dee2228031e4ef5b03d0c5734aef866?rik=BD%2bnjbFbllVmEQ&riu=http%3a%2f%2fimg.zcool.cn%2fcommunity%2f01cf02554336f10000019ae9df1dad.jpg%403000w_1l_2o_100sh.jpg&ehk=zvcYgjHlqK2U2x9ploUbmiBIk7BewUd6lyA0AIswegQ%3d&risl=&pid=ImgRaw&r=0',
+        text: '测试文字6',
+      },
+      {
+        src:
+          'https://p2.music.126.net/va9D07KDeS1ovOYAsoXE9A==/7929677859630995.jpg',
+        text: '测试文字',
+      },
+      {
+        src:
+          'https://ts1.cn.mm.bing.net/th/id/R-C.f40ba86561918519b95431a5921e4f5d?rik=9AIbo9AhOYel0w&riu=http%3a%2f%2fwww.quazero.com%2fuploads%2fallimg%2f131210%2f1-131210210248.jpg&ehk=v81JiWKphT%2baLBzbhrxRkTUUwwnhJ5F2PFkm4xn4nEM%3d&risl=&pid=ImgRaw&r=0',
+        text: '测试文字5',
+      },
+      {
+        src:
+          'https://ts1.cn.mm.bing.net/th/id/R-C.0dee2228031e4ef5b03d0c5734aef866?rik=BD%2bnjbFbllVmEQ&riu=http%3a%2f%2fimg.zcool.cn%2fcommunity%2f01cf02554336f10000019ae9df1dad.jpg%403000w_1l_2o_100sh.jpg&ehk=zvcYgjHlqK2U2x9ploUbmiBIk7BewUd6lyA0AIswegQ%3d&risl=&pid=ImgRaw&r=0',
+        text: '测试文字6',
+      },
+      {
+        src:
+          'https://ts1.cn.mm.bing.net/th/id/R-C.b0ea268fa1be279d112489ce83ad4696?rik=qItsh%2fBiy33hlg&riu=http%3a%2f%2fwww.quazero.com%2fuploads%2fallimg%2f140303%2f1-140303215009.jpg&ehk=S6PLWamt%2bMzQV8uO9ugcU5d5M19BpXtCpNz2cRJ7q9M%3d&risl=&pid=ImgRaw&r=0',
+        text: '测试文字2',
       },
     ];
     //使用定时器模拟HTTP请求，延时1s返回数据

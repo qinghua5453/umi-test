fdfbeff70b8d9161ca93c76b15b375aba95e7369
张健 <jian.zhang@chiefclouds.com>
2024-07-20 00:57:11 +0800

    递归

--- src/pages/arrTest/index.js
+++ src/pages/arrTest/index.js
@@ -4,34 +4,103 @@ import multData from './data';
 import TestCmp from './cmp';
 
 function ArrTest() {
-  const [a, setA] = useState(0);
-  const [b, setB] = useState(0);
-  const [c, setC] = useState(0);
-  const [flag1, setFlag1] = useState(false);
-  const [flag2, setFlag2] = useState(false);
+  const arr = [
+    {
+      id: 1,
+      parentId: null,
+    },
+    {
+      id: 2,
+      parentId: 1,
+    },
+    {
+      id: 3,
+      parentId: 2,
+    },
+    {
+      id: 4,
+      parentId: 3,
+    },
+    {
+      id: 5,
+      parentId: 3,
+    },
+    {
+      id: 6,
+      parentId: 2,
+    },
+    {
+      id: 6,
+      parentId: 2,
+    },
+    {
+      id: 7,
+      parentId: 4,
+    },
+  ];
 
-  const [abUpdated, setAbUpdated] = useState(false);
+  // 返回一个映射, 输入包含父子关系的数量
+  // const obj = {
+  //   1: 5,
+  //   2: 2,
+  //   3: 1,
+  //   4: 2,
+  // };
+  const obj = {};
 
-  useEffect(() => {
-    // 监听a和b的更新
-    if (flag1 && flag2) {
-      // console.log('555555')
-    }
-  }, [flag1, flag2]);
+  //  const buildArr = (data, type = 'slef', parentId = null) => {
+  //   for ( let i = 0; i < data.length; i++) {
+  //      const item = data[i];
+  //      if (!obj[item.id]) obj[item.id] = 0;
+  //      if (type === 'self') {
+  //       obj[item.id]++;
+  //      }
+  //      if (item.id === parentId && type === 'parent') {
+  //       obj[item.id]++
+  //       break;
+  //      }
+  //      if (item.parentId) {
+  //        buildArr(data, 'parent', item.parentId)
+  //      }
+  //   };
+  //  }
 
-  // console.log('这是外面的打印')
-  useEffect(() => {
-    setTimeout(() => {
-      setA(1);
-      setFlag1(true);
-      setC(1);
+  const buildArr = (data) => {
+    const map = {};
+
+    // 创建一个映射，方便查找每个节点的子节点
+    data.forEach((item) => {
+      if (!map[item.id]) {
+        map[item.id] = [];
+      }
+      if (item.parentId !== null) {
+        if (!map[item.parentId]) {
+          map[item.parentId] = [];
+        }
+        map[item.parentId].push(item.id);
+      }
     });
+    console.log('map', map);
 
-    // setTimeout(() => {
-    //   setB(1)
-    //   setFlag2(true)
-    // }, 3000)
-  }, []);
+    // 递归函数，计算子集数量
+    const countChildren = (id) => {
+      let count = 1; // 包括自身
+      if (map[id]) {
+        for (const childId of map[id]) {
+          count += countChildren(childId);
+        }
+      }
+      return count;
+    };
+
+    // 遍历每个节点，计算子集数量
+    for (const item of data) {
+      obj[item.id] = countChildren(item.id);
+    }
+  };
+
+  buildArr(arr);
+  console.log('dd', obj);
 
   return <TestCmp />;
 }
